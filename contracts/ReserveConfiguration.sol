664e6b5f506b14165124c13bcd4ef52a17d6eb91
smartdev <smartdevs17@gmail.com>
2026-01-20 15:55:16 +0100

    feat(contracts): add ReserveConfiguration library
    - Bitmask getters for reserve config
    - Efficient data packing accessors

---
+++ contracts/ReserveConfiguration.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./DataTypes.sol";
+
+library ReserveConfiguration {
+    function getLtv(DataTypes.ReserveConfigurationMap memory self) internal pure returns (uint256) {
+        return self.data & 0xFFFF;
+    }
+    
+    function getLiquidationThreshold(DataTypes.ReserveConfigurationMap memory self) internal pure returns (uint256) {
+        return (self.data >> 16) & 0xFFFF;
+    }
+    
+    function getLiquidationBonus(DataTypes.ReserveConfigurationMap memory self) internal pure returns (uint256) {
+        return (self.data >> 32) & 0xFFFF;
+    }
+    
+    function getDecimals(DataTypes.ReserveConfigurationMap memory self) internal pure returns (uint256) {
+        return (self.data >> 48) & 0xFF;
+    }
+    
+    function getActive(DataTypes.ReserveConfigurationMap memory self) internal pure returns (bool) {
+        return (self.data >> 56) & 1 != 0;
+    }
+}
