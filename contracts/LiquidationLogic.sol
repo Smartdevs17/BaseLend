508124c60a3021d9bb100eab80feac82c2f6c3b8
smartdev <smartdevs17@gmail.com>
2026-01-28 10:48:15 +0100

    docs: add NatSpec for LiquidationLogic library

--- contracts/LiquidationLogic.sol
+++ contracts/LiquidationLogic.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/**
+ * @title LiquidationLogic
+ * @notice Implements the logic for calculating liquidation amounts
+ */
 library LiquidationLogic {
+    /**
+     * @notice Calculates the max amount (debt + bonus) a liquidator can seize
+     * @param debt The total debt amount to be covered
+     * @param collateral The available collateral of the borrower
+     * @param liquidationBonus The bonus percentage in basis points (e.g. 500 = 5%)
+     * @return The maximum collateral amount to be liquidated
+     */
     function calculateLiquidationAmount(
         uint256 debt,
         uint256 collateral,
